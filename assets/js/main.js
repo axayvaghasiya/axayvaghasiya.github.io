dd69a18f5943868c887798ffe2acaac53da85bc7
Akshay Vaghasiya <44130033+axayvaghasiya@users.noreply.github.com>
2025-07-29 00:01:29 +0100

    Update main.js
    Added Sparkle effect on mouse move

--- assets/js/main.js
+++ assets/js/main.js
@@ -332,3 +332,30 @@
       $main._show(location.hash.substr(1), true);
     });
 })(jQuery);
+
+/ Add some interactive sparkle effects on mouse move
+        document.addEventListener('mousemove', function(e) {
+            if (Math.random() > 0.95) {
+                createSparkle(e.clientX, e.clientY);
+            }
+        });
+        
+        function createSparkle(x, y) {
+            const sparkle = document.createElement('div');
+            sparkle.style.position = 'fixed';
+            sparkle.style.left = x + 'px';
+            sparkle.style.top = y + 'px';
+            sparkle.style.width = '3px';
+            sparkle.style.height = '3px';
+            sparkle.style.background = 'rgba(56, 189, 248, 0.8)';
+            sparkle.style.borderRadius = '50%';
+            sparkle.style.pointerEvents = 'none';
+            sparkle.style.zIndex = '1000';
+            sparkle.style.animation = 'sparkle 1s ease-out forwards';
+            
+            document.body.appendChild(sparkle);
+            
+            setTimeout(() => {
+                sparkle.remove();
+            }, 1000);
+        }
